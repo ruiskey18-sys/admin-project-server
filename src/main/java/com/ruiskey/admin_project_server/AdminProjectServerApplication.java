d499d10bb92cb19b3e0f9a78afd3a04db761ac53
ruiskey <18130319287@163.com>
2026-08-08 16:13:08 +0800

    springboot-初始化

---
+++ src/main/java/com/ruiskey/admin_project_server/AdminProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.ruiskey.admin_project_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminProjectServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruiskey/admin_project_server/AdminProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruiskey.admin_project_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
